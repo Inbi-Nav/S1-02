befb1436a9490ff0c11d30f151236311a5c72034
Inbisat Naveed <inkyn2002@gmail.com>
2025-11-03 12:27:36 +0100

    add exercise 1 - variable and constants

---
+++ Nivel-1/exercicis-N1.php
@@ -0,0 +1,20 @@
+<h1 style="color: darkblue;">Exercici 1</h1>
+<?php
+$age = 40;
+$percentage = 77.89;
+$name = "Emma";
+$IsStudent = "False";
+
+echo "<pre>";
+print($age);
+echo "<pre>";
+print($percentage);
+echo "<pre>";
+print($name);
+echo "<pre>";
+print($IsStudent);
+
+const MY_NAME = 'Inbi';
+?>
+
+<h1><?= MY_NAME; ?></h1>
