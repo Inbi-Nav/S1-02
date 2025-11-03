0cc9889d4fdb9568d9d2bfe0b877f32f4559493c
Inbisat Naveed <inkyn2002@gmail.com>
2025-11-03 23:48:57 +0100

    exercise 2 Nivel 2: : calculate total player points, average, player's classification

--- Nivel-2/exercicis-N2.php
+++ Nivel-2/exercicis-N2.php
@@ -16,3 +16,39 @@ function CalcularLlamada ($tiempoLlamada, $pagar) {
     $pagar = 10;
     echo CalcularLlamada(6, $pagar)
 ?>
+
+<h1 style="color: darkblue;">Exercici 2 Nivel 2</h1>
+
+<?php
+
+$a = rand(0, 9999);
+$b = rand(0, 9999);
+$c = rand(0, 9999);
+$SumaPuntuacion = CalcularSuma($a, $b, $c);
+
+function CalcularSuma ($a, $b, $c) {
+
+   $SumaPuntuacion = $a + $b + $c;
+   return $SumaPuntuacion;
+    
+}
+
+function CalcularMedia ($SumaPuntuacion){
+    $media =  $SumaPuntuacion / 3;
+    return $media;
+}
+
+function Clasificacion ($SumaPuntuacion) {
+    if ($SumaPuntuacion < 4000) {
+        return "Principante";
+    } else if ($SumaPuntuacion < 8000)
+        return "Intermedio";
+        else {
+            return "Professional";
+        }
+}
+
+echo "<strong>La suma total de puntuacion es: </strong>" . CalcularSuma($a, $b, $c) ; 
+echo "<br><strong> La media total es: </strong>" . CalcularMedia($SumaPuntuacion);
+echo "<br><strong> Clasificacion: </strong>" . Clasificacion($SumaPuntuacion);
+?>
